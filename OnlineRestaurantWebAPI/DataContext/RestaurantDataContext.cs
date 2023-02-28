25152dbc789b24b155de5ac17e5d0a031ca8d32b
Maheel <Maheellakshanra@gmail.com>
2023-03-01 01:13:51 +0530

    Add project files.

---
+++ OnlineRestaurantWebAPI/DataContext/RestaurantDataContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineRestaurantWebAPI.DataModels;
+
+namespace OnlineRestaurantWebAPI.DataContext
+
+{
+    public class RestaurantDataContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Crew> Crews { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace the connection string with your own database connection string
+            optionsBuilder.UseSqlServer("Server=localhost;Database=RestaurantDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasKey(c => c.cust_ID);
+            modelBuilder.Entity<Customer>().HasMany(c => c.OrderDetails).WithOne(od => od.Customer);
+
+            modelBuilder.Entity<Crew>().HasKey(c => c.crew_ID);
+            modelBuilder.Entity<Crew>().HasMany(c => c.OrderDetails).WithOne(od => od.Crew);
+
+            modelBuilder.Entity<OrderDetails>().HasKey(od => od.order_ID);
+            modelBuilder.Entity<OrderDetails>().HasOne(od => od.Crew).WithMany(c => c.OrderDetails).HasForeignKey(od => od.crew_ID);
+            modelBuilder.Entity<OrderDetails>().HasOne(od => od.Customer).WithMany(c => c.OrderDetails).HasForeignKey(od => od.cust_ID);
+            modelBuilder.Entity<OrderDetails>().HasOne(od => od.Food).WithMany(f => f.OrderDetails).HasForeignKey(od => od.food_ID);
+
+            modelBuilder.Entity<Payment>().HasKey(p => p.pay_ID);
+            modelBuilder.Entity<Payment>().HasOne(p => p.Customer).WithMany(c => c.Payments).HasForeignKey(p => p.cust_ID);
+            modelBuilder.Entity<Payment>().HasOne(p => p.OrderDetails).WithMany(od => od.Payments).HasForeignKey(p => p.order_ID);
+            modelBuilder.Entity<Payment>().HasOne(p => p.Delivery).WithMany(d => d.Payments).HasForeignKey(p => p.delivery_ID);
+
+            modelBuilder.Entity<Menu>().HasKey(m => m.menu_ID);
+            modelBuilder.Entity<Menu>().HasOne(m => m.Food).WithMany(f => f.Menus).HasForeignKey(m => m.food_ID);
+
+            modelBuilder.Entity<Food>().HasKey(f => f.food_ID);
+
+            modelBuilder.Entity<Delivery>().HasKey(d => d.delivery_ID);
+            modelBuilder.Entity<Delivery>().HasOne(d => d.OrderDetails).WithOne(od => od.Delivery).HasForeignKey<Delivery>(d => d.order_ID);
+
+            modelBuilder.Entity<Transaction>().HasKey(t => t.trans_ID);
+            modelBuilder.Entity<Transaction>().HasOne(t => t.Customer).WithMany(c => c.Transactions).HasForeignKey(t => t.cust_ID);
+            modelBuilder.Entity<Transaction>().HasOne(t => t.Crew).WithMany(c => c.Transactions).HasForeignKey(t => t.crew_ID);
+            modelBuilder.Entity<Transaction>().HasOne(t => t.OrderDetails).WithMany(od => od.Transactions).HasForeignKey(t => t.order_ID);
+        }
+    }
+
+   
+
+
+
+
+
+   
+}
---
+++ OnlineRestaurantWebAPI/DataModels/DataModels.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineRestaurantWebAPI.DataModels
+{
+    public class DataModels
+    {
+    }
+    public class Customer
+    {
+        [Key]
+        public int cust_ID { get; set; }
+        public string name { get; set; }
+        public int contact { get; set; }
+        public string address { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+        public ICollection<Payment> Payments { get; set; }
+    }
+
+    public class Crew
+    {
+        [Key]
+        public int crew_ID { get; set; }
+        public string name { get; set; }
+        public int contact { get; set; }
+        public string address { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+
+    public class OrderDetails
+    {
+        [Key]
+        public int order_ID { get; set; }
+        public int crew_ID { get; set; }
+        public int cust_ID { get; set; }
+        public int food_ID { get; set; }
+        public DateTime date { get; set; }
+        public string status { get; set; }
+        public Crew Crew { get; set; }
+        public Customer Customer { get; set; }
+        public Food Food { get; set; }
+        public Payment Payments { get; set; }
+    }
+
+    public class Payment
+    {
+        [Key]
+        public int pay_ID { get; set; }
+        public int cust_ID { get; set; }
+        public int order_ID { get; set; }
+        public int delivery_ID { get; set; }
+        public int amount { get; set; }
+        public DateTime date { get; set; }
+        public Customer Customer { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+        public Delivery Delivery { get; set; }
+        
+    }
+
+    public class Menu
+    {
+        [Key]
+        public int menu_ID { get; set; }
+        public int food_ID { get; set; }
+        public string details { get; set; }
+        public Food Food { get; set; }
+    }
+
+    public class Food
+    {
+        [Key]
+        public int food_ID { get; set; }
+        public string name { get; set; }
+        public int status { get; set; }
+        public int price { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+        public ICollection<Menu> Menus { get; set; }
+    }
+
+    public class Delivery
+    {
+        [Key]
+        public int delivery_ID { get; set; }
+        public int order_ID { get; set; }
+        public DateTime date { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+    }
+
+    public class Transaction
+    {
+        [Key]
+        public int trans_ID { get; set; }
+        public DateTime trans_date { get; set; }
+        public int cust_ID { get; set; }
+        public int crew_ID { get; set; }
+        public int order_ID { get; set; }
+        public DateTime report_date { get; set; }
+        public Customer Customer { get; set; }
+        public Crew Crew { get; set; }
+        public OrderDetails OrderDetails { get; set; }
+    }
+}
